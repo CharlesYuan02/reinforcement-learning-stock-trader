ab71eaf1de5f574132968f1d730d3f3c89fa4fe0
Charles Yuan <charlesyuan59@gmail.com>
2023-04-14 01:17:18 -0400

    Initial commit

---
+++ custom_environment_multistock.py
@@ -0,0 +1,145 @@
+import gym
+import numpy as np
+from gym import spaces
+
+
+class CustomStockTradingEnv(gym.Env):
+    def __init__(self, dfs, window_size=10, k=1000, num_features=6, starting_balance=100000):
+        '''
+        df: dataframe of multiple stocks concatenated together (shape (num_stocks, num_days, num_features))
+        window_size: number of previous days to consider
+        k: max number of shares to buy or sell
+        num_features: number of features to consider (i.e. Number of columns in dataframe not including date)
+        starting_balance: starting balance of account - the higher this is, the more leeway the agent has to make mistakes and learn
+        __init__ should initialize the action space and observation space        
+        '''
+        self.dfs = dfs
+        self.window_size = window_size
+        self.k = k
+        self.num_stocks = len(dfs) # Number of stocks in the portfolio
+        self.action_space = spaces.Box(low=-1, high=1, shape=(self.num_stocks, 1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-k, high=k, shape=(self.num_stocks, window_size, num_features), dtype=np.float32)
+        self.starting_balance = starting_balance
+        self.reset()
+    
+    def reset(self):
+        '''
+        Include the following lists over the training period:
+        self.profits: list of profits at each step
+        self.total_profits: list of cumulative profits at each step
+        self.shares: list of number of shares owned at each step
+        self.account_balance: list of account balance at each step
+        self.rewards: list of rewards at each step
+        etc
+        '''
+        self.current_step = self.window_size
+        self.total_portfolio_value = [self.starting_balance] # This is the account balance + value of shares owned at each iteration
+        self.account_balance = [self.starting_balance]
+        self.rewards = []
+
+        # This is the total number of shares owned for each stock for each iteration
+        # It will have shape (num_iterations, num_stocks)
+        zeros_list = [0] * self.num_stocks
+        self.num_shares = [zeros_list]
+        self.trades = 0 # Extra variable to track number of trades
+
+        # Technical indicators - we want them in shape (num_stocks, num_iterations)
+        self.prices = np.array([df["Close"].values for df in self.dfs])
+        self.macd = np.array([df["MACD"].values for df in self.dfs])
+        self.signal = np.array([df["Signal"].values for df in self.dfs])
+        self.rsi = np.array([df["RSI"].values for df in self.dfs])
+        self.cci = np.array([df["CCI"].values for df in self.dfs])
+        self.adx = np.array([df["ADX"].values for df in self.dfs])
+        return self._next_observation()
+    
+    def _next_observation(self):
+        # We need the obs to be of shape (window_size, num_features, num_stocks)
+        # obs starts off as shape (num_features, num_stocks, window_size)
+        obs = np.array([self.prices[:, self.current_step - self.window_size:self.current_step],
+                        self.macd[:, self.current_step - self.window_size:self.current_step],
+                        self.signal[:, self.current_step - self.window_size:self.current_step],
+                        self.rsi[:, self.current_step - self.window_size:self.current_step],
+                        self.cci[:, self.current_step - self.window_size:self.current_step],
+                        self.adx[:, self.current_step - self.window_size:self.current_step]])
+        return np.transpose(obs, (1, 2, 0))
+    
+    def step(self, action):
+        self._take_action(action)
+
+        self.current_step += 1
+
+        # np.array(self.dfs).shape[1] is the number of days in the dataset
+        if self.current_step >= np.array(self.dfs).shape[1] - 1:
+            done = True
+        else:
+            done = False
+
+        obs = self._next_observation()
+        return obs, sum(self.rewards), done, {"account_balance": self.account_balance, "num_shares": self.num_shares, "total_portfolio_value": self.total_portfolio_value}
+    
+    def calculate_reward(self):
+        '''
+        Calculate reward based on two factors:
+        1. account_balance[-1] - account_balance[-2]
+            * This includes the profit made upon selling some shares
+        2. Current value of the portfolio (i.e. num_shares * current_price)
+        '''
+        # raw_reward = self.total_portfolio_value[-1] - self.total_portfolio_value[-2] # Basically the same thing after clipping
+        # self._get_current_price() returns an array of size (num_stocks, 1)
+        # Do element-wise multiplication for the amount of each stock at most recent time step with the current prices for each stock
+        current_portfolio_value = np.sum(np.array(self.num_shares[-1]) * np.array(self._get_current_price()))
+        raw_reward = self.account_balance[-1] - self.account_balance[-2] + current_portfolio_value - self.total_portfolio_value[-1]
+        reward = np.clip(raw_reward, -1, 1) # Clip reward to be between -1 and 1
+        return reward
+    
+    def _take_action(self, action):
+        current_price = self._get_current_price() # The closing price for the day (list of size (num_stocks, 1))
+        actions = action # Should be a list of floats between -1 and 1
+
+        # Append these after looping through all the stocks during this iteration
+        new_account_balance = self.account_balance[-1] # Scalar
+        new_num_shares = self.num_shares[-1].copy() # List
+        new_total_portfolio_value = self.total_portfolio_value[-1] # Scalar
+
+        # Loop through each stock and perform buy/sell action
+        for i in range(len(actions)): # For each stock, do the same thing as for single-stock training
+            # Buy if action > 0 and if you have enough money
+            if actions[i] > 0 and self.account_balance[-1] > current_price[i]:
+                # Convert float to number of shares based on set "k" value (max number of shares to buy)
+                # If you don't have enough money, just buy as many as you can
+                # Since we have multiple stocks, just use a simple rule and divide k by the number of stocks
+                k_new = self.k / self.num_stocks
+                shares_bought = min(int(self.account_balance[-1] / current_price[i]), int(actions[i] * k_new))
+                new_account_balance -= shares_bought * current_price[i]
+                new_num_shares[i] += shares_bought # This is the new number of shares for this stock
+                new_total_portfolio_value += shares_bought * current_price[i]
+                self.trades += 1
+                
+            # Sell if action < 0 and if you have enough shares
+            elif actions[i] < 0 and self.num_shares[-1][i] > 0:
+                # Convert float to number of shares based on set "k" value (max number of shares to sell)
+                # If you don't have enough shares, just sell as many as you can
+                k_new = self.k / self.num_stocks
+                shares_sold = min(self.num_shares[-1][i], int(-actions[i] * k_new))
+                new_account_balance += shares_sold * current_price[i]
+                new_num_shares[i] -= shares_sold # This is the new number of shares for this stock
+                new_total_portfolio_value -= shares_sold * current_price[i]
+                self.trades += 1
+            
+            # Otherwise, just hold
+            else:
+                # new_account_balance += 0
+                # new_num_shares[i] = new_num_shares[i]
+                new_total_portfolio_value += new_num_shares[i] * current_price[i]
+        
+        # Append the new values to the lists
+        self.account_balance.append(new_account_balance)
+        self.num_shares.append(new_num_shares)
+        self.total_portfolio_value.append(new_total_portfolio_value)
+        self.rewards.append(self.calculate_reward()) # Cuz we need to calculate it based on the updated global arrays
+
+    def _get_current_price(self):
+        '''
+        Return the closing prices for each stock at the current step
+        '''
+        return self.prices[:, self.current_step]
---
+++ train_multi_stock.py
@@ -0,0 +1,159 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from stable_baselines3 import A2C, PPO
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.vec_env import DummyVecEnv
+from custom_environment_multistock import CustomStockTradingEnv
+
+
+def train(stocks, start_date, end_date, 
+          model_name="PPO", features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
+          window_size=10, k_value=1000, starting_balance=100000, gamma=0.99, num_timesteps=250):
+    '''
+    This function will train either A2C or PPO using the custom environment created in custom_environment.py.
+    The model will be saved and can be loaded later for evaluation.
+
+    Parameters:
+    stocks: list of stock tickers (strings)
+    start_date: string of start of training period in format "YYYY-MM-DD"
+    end_date: string of end of training period in format "YYYY-MM-DD"
+    model: string of model to use, either "PPO" or "A2C"
+    features: list of features to use, default is what you see
+    window_size: int of window size to use, default is 10
+    k_value: int of max number of shares to buy/sell at a time
+    starting_balance: int of starting balance of account    
+    '''
+    dfs = [] # Shape (num_stocks, num_days, num_features)
+    for stock in stocks:
+        df = pd.read_csv(f'data/{stock}.csv')
+        df = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
+        df = df[features]
+        df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+        df.set_index("Date", inplace=True)
+        dfs.append(df)
+
+    env = CustomStockTradingEnv(dfs, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    
+    if model_name == "PPO":
+        model = PPO("MlpPolicy", env, gamma=gamma, verbose=0)
+    elif model_name == "A2C":
+        model = A2C("MlpPolicy", env, gamma=gamma, verbose=0)
+    else:
+        raise ValueError("Please select PPO or A2C")
+    # eval_callback = EvalCallback(env, eval_freq=100, n_eval_episodes=5)
+    # model.learn(total_timesteps=num_timesteps, callback=[eval_callback])
+    model.learn(total_timesteps=num_timesteps)
+    
+    # Save trained model (this is not the same way you save a Tensorflow model)
+    if not os.path.exists("models"):
+        os.mkdir("models")
+    model.save(f"models/multistock_{model_name}")
+    print(f"Model saved as multistock_{model_name}")
+
+    obs = env.reset()
+    for i in range(np.array(dfs).shape[1]):
+        action, _ = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances = info['account_balance']
+            num_shares = info['num_shares']
+            total_portfolio_value = info['total_portfolio_value']
+            break
+
+    print("Account balance: {}".format(account_balances[-1]))
+    print("Number of shares: {}".format(num_shares[-1]))
+    print("Total portfolio value: {}".format(total_portfolio_value[-1]))
+
+    # Plot training results
+    plt.figure(figsize=(15, 6))
+    plt.plot(total_portfolio_value, label='Portfolio value')
+    plt.title(f"Portfolio Value, Multistock")
+    plt.xlabel(f"Day {start_date} - {end_date}")
+    plt.ylabel("Portfolio Value ($)")
+
+    # Save plot
+    if not os.path.exists("plots"):
+        os.mkdir("plots")
+    plt.savefig(f"plots/training_multistock_{model_name}.png")
+    plt.show()
+
+
+def evaluate(stocks, start_date, end_date, trained_model, 
+             features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
+             window_size=10, k_value=1000, starting_balance=100000):
+    '''
+    Load the saved model from the path "trained_model" and evaluate it on the testing data.
+    The testing data should be a period of time after the training data that the model has not seen.
+    '''
+    dfs = []
+    for stock in stocks:
+        df = pd.read_csv(f'data/{stock}.csv')
+        df = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
+        df = df[features]
+        df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+        df.set_index("Date", inplace=True)
+        dfs.append(df)
+    df_concat = pd.concat(dfs, axis=1)
+
+    # Create the environment used to test the agent
+    env = CustomStockTradingEnv(df_concat, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    if trained_model.endswith("PPO"):
+        model = PPO.load(trained_model)
+    elif trained_model.endswith("A2C"):
+        model = A2C.load(trained_model)
+    else:
+        raise ValueError("Please select PPO or A2C")
+
+    obs = env.reset()
+    for i in range(len(df_concat[0])):
+        action, _ = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances = info['account_balance']
+            num_shares = info['num_shares']
+            total_portfolio_value = info['total_portfolio_value']
+            break
+
+    # Length is len(df) - window_size = 46
+    # print(len(df), len(total_portfolio_value))
+
+    print("Account balance: {}".format(account_balances[-1]))
+    print("Number of shares: {}".format(num_shares[-1]))
+    print("Total portfolio value: {}".format(total_portfolio_value[-1]))
+
+    # Plot testing results
+    plt.figure(figsize=(15, 6))
+    plt.plot(total_portfolio_value, label='Portfolio value')
+    plt.title(f"Portfolio Value, Multistock")
+    plt.xlabel(f"Day {start_date} - {end_date}")
+    plt.ylabel("Portfolio Value ($)")
+    plt.savefig(f"plots/testing_multistock_{trained_model.split('_')[-1]}.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Training 
+    stocks = ["AAPL", "MSFT", "AMZN", "NVDA", "GOOGL"]
+    start_train = "2021-01-01"
+    end_train = "2023-01-01"
+    model = "PPO"
+    features = ["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"]
+    window_size = 10
+    k_value = 1000
+    starting_balance = 100000
+    gamma = 0.99
+    num_timesteps = 250
+    train(stocks, start_train, end_train, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
+    quit()
+
+    # Evaluation
+    start_test = "2023-01-01"
+    end_test = None
+    trained_model = f"models/multistock_{model}"
+    evaluate(stocks, start_test, end_test, trained_model, features, window_size, k_value, starting_balance)
