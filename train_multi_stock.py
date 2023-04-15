207f10ef2a592c8bf3450bf0c6e28a14214aee28
Charles Yuan <charlesyuan59@gmail.com>
2023-04-15 16:57:14 -0400

    Added functions to compare PPO and A2C and display portfolio allocations

--- train_multi_stock.py
+++ train_multi_stock.py
@@ -6,9 +6,10 @@ from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.vec_env import DummyVecEnv
 from custom_environment_multistock import CustomStockTradingEnv
+from data_extractor import get_stock_names
 
 
-def plot_portfolio(num_shares, stocks, start_date, end_date):
+def plot_portfolio(num_shares, stocks, start_date, end_date, name):
     '''
     Given the array of number of shares, plot the amount of each stock
     in the portfolio over time.    
@@ -17,15 +18,15 @@ def plot_portfolio(num_shares, stocks, start_date, end_date):
     num_shares = np.array(num_shares)
     for i in range(num_shares.shape[1]):
         plt.plot(range(num_shares.shape[0]), num_shares[:, i], label=stocks[i])
-    plt.title(f"Portfolio Shares")
+    plt.title(f"Portfolio Shares, {name}")
     plt.xlabel(f"Day {start_date} - {end_date}")
     plt.ylabel("Number of Shares")
     plt.legend()
-    plt.savefig(f"plots/portfolio_shares.png")
+    plt.savefig(f"plots/portfolio_shares_{name}.png")
     plt.show()
 
 
-def train(stocks, start_date, end_date, 
+def train(stocks, start_date, end_date, training_period_length,
           model_name="PPO", features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
           window_size=10, k_value=1000, starting_balance=100000, gamma=0.99, num_timesteps=250):
     '''
@@ -44,7 +45,10 @@ def train(stocks, start_date, end_date,
     '''
     dfs = [] # Shape (num_stocks, num_days, num_features)
     for stock in stocks:
+        # Do not use stocks that do not have enough data
         df = pd.read_csv(f'data/{stock}.csv')
+        if len(df) < training_period_length:
+            continue
         df = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
         df = df[features]
         df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
@@ -99,7 +103,7 @@ def train(stocks, start_date, end_date,
     plt.show()
 
 
-def evaluate(stocks, start_date, end_date, trained_model, 
+def evaluate(stocks, start_date, end_date, testing_period_length, trained_model, 
              features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
              window_size=10, k_value=1000, starting_balance=100000):
     '''
@@ -109,6 +113,8 @@ def evaluate(stocks, start_date, end_date, trained_model,
     dfs = []
     for stock in stocks:
         df = pd.read_csv(f'data/{stock}.csv')
+        if len(df) < testing_period_length:
+            continue
         if end_date is None or end_date == "Present":
             df = df[df["Date"] >= start_date]
             end_date = "Present"
@@ -127,7 +133,10 @@ def evaluate(stocks, start_date, end_date, trained_model,
         model = A2C.load(trained_model)
     else:
         raise ValueError("Please select PPO or A2C")
-
+    
+    # Remember to set random seed for reproducibility
+    model.set_random_seed(0)
+    
     obs = env.reset()
     for i in range(np.array(dfs).shape[1]):
         action, _ = model.predict(obs)
@@ -142,6 +151,7 @@ def evaluate(stocks, start_date, end_date, trained_model,
     # Length is len(df) - window_size = 46
     # print(len(df), len(total_portfolio_value))
 
+    print("Model: {}".format(trained_model))
     print("Account balance: {}".format(account_balances[-1]))
     print("Number of shares: {}".format(num_shares[-1]))
     print("Total portfolio value: {}".format(total_portfolio_value[-1]))
@@ -149,31 +159,138 @@ def evaluate(stocks, start_date, end_date, trained_model,
     # Plot testing results
     plt.figure(figsize=(15, 6))
     plt.plot(total_portfolio_value, label='Portfolio value')
-    plt.title(f"Portfolio Value, Multistock")
+    plt.title(f"Portfolio Value, {trained_model[-3:]} Multistock")
     plt.xlabel(f"Day {start_date} - {end_date}")
     plt.ylabel("Portfolio Value ($)")
     plt.savefig(f"plots/testing_multistock_{trained_model.split('_')[-1]}.png")
     plt.show()
 
-    plot_portfolio(num_shares, stocks, start_date, end_date)
+    plot_portfolio(num_shares, stocks, start_date, end_date, trained_model[-3:])
 
+
+def evaluate_both(stocks, start_date, end_date, testing_period_length, trained_model1, trained_model2,
+             features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
+             window_size=10, k_value=1000, starting_balance=100000):
+    '''
+    Load the saved model from the path "trained_model" and evaluate it on the testing data.
+    The testing data should be a period of time after the training data that the model has not seen.
+    '''
+    dfs = []
+    for stock in stocks:
+        df = pd.read_csv(f'data/{stock}.csv')
+        if len(df) < testing_period_length:
+            continue
+        if end_date is None or end_date == "Present":
+            df = df[df["Date"] >= start_date]
+            end_date = "Present"
+        else:
+            df = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
+        df = df[features]
+        df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+        df.set_index("Date", inplace=True)
+        dfs.append(df)
+
+    # Test model 1
+    env = CustomStockTradingEnv(dfs, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    if trained_model1.endswith("PPO"):
+        model1 = PPO.load(trained_model1)
+    elif trained_model1.endswith("A2C"):
+        model1 = A2C.load(trained_model1)
+    else:
+        raise ValueError("Please select PPO or A2C")
+    
+    # Remember to set random seed for reproducibility
+    model1.set_random_seed(0)
+    
+    obs = env.reset()
+    for i in range(np.array(dfs).shape[1]):
+        action, _ = model1.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances1 = info['account_balance']
+            num_shares1 = info['num_shares']
+            total_portfolio_value1 = info['total_portfolio_value']
+            break
+    
+    # Test model 2
+    env = CustomStockTradingEnv(dfs, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    if trained_model2.endswith("PPO"):
+        model2 = PPO.load(trained_model2)
+    elif trained_model2.endswith("A2C"):
+        model2 = A2C.load(trained_model2)
+    else:
+        raise ValueError("Please select PPO or A2C")
+    
+    # Remember to set random seed for reproducibility
+    model2.set_random_seed(0)
+    
+    obs = env.reset()
+    for i in range(np.array(dfs).shape[1]):
+        action, _ = model2.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances2 = info['account_balance']
+            num_shares2 = info['num_shares']
+            total_portfolio_value2 = info['total_portfolio_value']
+            break
+
+    # Plot testing results
+    plt.figure(figsize=(15, 6))
+    plt.plot(total_portfolio_value1, label='Portfolio Value (A2C)')
+    plt.plot(total_portfolio_value2, label='Portfolio Value (PPO)')
+    plt.title(f"Portfolio Value, Multistock")
+    plt.xlabel(f"Day {start_date} - {end_date}")
+    plt.ylabel("Portfolio Value ($)")
+    plt.legend(loc="best")
+    plt.savefig(f"plots/testing_multistock_ensemble.png")
+    plt.show()
 
 if __name__ == "__main__":
     # Training 
-    stocks = ["AAPL", "MSFT", "AMZN", "NVDA", "GOOGL", "TSLA", "AMD", "EA", "QCOM", "ADBE"]
+    stocks = ["AAPL", "MSFT", "NVDA", "AMZN", "GOOGL"] 
+    # stocks = get_stock_names()
     start_train = "2021-01-01"
     end_train = "2023-01-01"
-    model = "PPO"
+
+    # We need to calculate the training period length to cut out stocks that don't have enough data
+    # We'll use AAPL as a reference
+    df = pd.read_csv(f'data/AAPL.csv')
+    df = df[(df["Date"] >= start_train) & (df["Date"] <= end_train)]
+    training_period_length = len(df)
+
+    model = "A2C"
     features = ["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"]
     window_size = 10
-    k_value = 1000 / len(stocks)
+    k_value = 100 / (2 * len(stocks))
     starting_balance = 100000
-    gamma = 0.99
+    gamma = 0.95
     num_timesteps = 250
-    # train(stocks, start_train, end_train, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
+    train(stocks, start_train, end_train, training_period_length, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
+
+    model = "PPO"
+    train(stocks, start_train, end_train, training_period_length, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
 
     # Evaluation
     start_test = "2023-01-01"
     end_test = None
-    trained_model = f"models/multistock_{model}"
-    evaluate(stocks, start_test, end_test, trained_model, features, window_size, k_value, starting_balance)
+    df = pd.read_csv(f'data/AAPL.csv')
+    if end_test is None or end_test == "Present":
+        df = df[df["Date"] >= start_test]
+        end_test = "Present"
+    else:
+        df = df[(df["Date"] >= start_test) & (df["Date"] <= end_test)]
+    testing_period_length = len(df)
+    trained_model = f"models/multistock_A2C"
+    evaluate(stocks, start_test, end_test, testing_period_length, trained_model, features, window_size, k_value, starting_balance)
+    
+    trained_model = f"models/multistock_PPO"
+    evaluate(stocks, start_test, end_test, testing_period_length, trained_model, features, window_size, k_value, starting_balance)
+
+    # Ensemble
+    trained_model1 = f"models/multistock_A2C"
+    trained_model2 = f"models/multistock_PPO"
+    evaluate_both(stocks, start_test, end_test, testing_period_length, trained_model1, trained_model2, features, window_size, k_value, starting_balance)
--- train_single_stock.py
+++ train_single_stock.py
@@ -103,6 +103,9 @@ def evaluate(stock, start_date, end_date, trained_model,
         model = A2C.load(trained_model)
     else:
         raise ValueError("Please select PPO or A2C")
+    
+    # Remember to set random seed for reproducibility
+    model.set_random_seed(0)
 
     obs = env.reset()
     for i in range(len(df)):
@@ -118,6 +121,7 @@ def evaluate(stock, start_date, end_date, trained_model,
     # Length is len(df) - window_size = 46
     # print(len(df), len(total_portfolio_value))
 
+    print("Model: {}".format(trained_model))
     print("Account balance: {}".format(account_balances[-1]))
     print("Number of shares: {}".format(num_shares[-1]))
     print("Total portfolio value: {}".format(total_portfolio_value[-1]))
@@ -132,22 +136,104 @@ def evaluate(stock, start_date, end_date, trained_model,
     plt.show()
 
 
+def evaluate_both(stock, start_date, end_date, trained_model1, trained_model2,
+             features=["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"], 
+             window_size=10, k_value=1000, starting_balance=100000):
+    '''
+    Load the saved model from the path "trained_model" and evaluate it on the testing data.
+    The testing data should be a period of time after the training data that the model has not seen.
+    '''
+    df = pd.read_csv(f'data/{stock}.csv')
+    if end_date is not None:
+        df = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
+    else:
+        df = df[(df["Date"] >= "2023-01-01")]
+        end_date = "Present"
+    df = df[features]
+    df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+    df.set_index("Date", inplace=True)
+
+    # We test the first trained model (A2C)
+    env = CustomStockTradingEnv(df, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    if trained_model1.endswith("PPO"):
+        model1 = PPO.load(trained_model1)
+    elif trained_model1.endswith("A2C"):
+        model1 = A2C.load(trained_model1)
+    else:
+        raise ValueError("Please select PPO or A2C")
+    
+    model1.set_random_seed(0)
+    obs = env.reset()
+    for i in range(len(df)):
+        action, _ = model1.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances1 = info['account_balance']
+            num_shares1 = info['num_shares']
+            total_portfolio_value1 = info['total_portfolio_value']
+            break
+
+    # Then we test the second trained model (PPO)
+    env = CustomStockTradingEnv(df, window_size=window_size, k=k_value, starting_balance=starting_balance)
+    env = DummyVecEnv([lambda: env])
+    if trained_model2.endswith("PPO"):
+        model2 = PPO.load(trained_model2)
+    elif trained_model2.endswith("A2C"):
+        model2 = A2C.load(trained_model2)
+    else:
+        raise ValueError("Please select PPO or A2C")
+    
+    model2.set_random_seed(0)
+    obs = env.reset()
+    for i in range(len(df)):
+        action, _ = model2.predict(obs)
+        obs, reward, done, info = env.step(action)
+        if done:
+            info = info[0]
+            account_balances2 = info['account_balance']
+            num_shares2 = info['num_shares']
+            total_portfolio_value2 = info['total_portfolio_value']
+            break
+
+    # Plot testing results
+    plt.figure(figsize=(7, 6))
+    plt.plot(total_portfolio_value1, label='Portfolio Value (A2C)')
+    plt.plot(total_portfolio_value2, label='Portfolio Value (PPO)')
+    plt.title(f"Portfolio Value, {stock} Stock")
+    plt.xlabel(f"Day {start_date} - {end_date}")
+    plt.ylabel("Portfolio Value ($)")
+    plt.legend(loc="best")
+    plt.savefig(f"plots/testing_{stock}_ensemble.png")
+    plt.show()
+
+
 if __name__ == "__main__":
     # Training 
-    stock = "TSLA"
+    stock = "GOOGL"
     start_train = "2021-01-01"
     end_train = "2023-01-01"
-    model = "PPO"
+    model = "A2C"
     features = ["Date", "Close", "MACD", "Signal", "RSI", "CCI", "ADX"]
     window_size = 10
-    k_value = 1000
+    k_value = 100
     starting_balance = 100000
-    gamma = 0.99
+    gamma = 0.95
     num_timesteps = 250
+    train(stock, start_train, end_train, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
+
+    model = "PPO"
     train(stock, start_train, end_train, model, features, window_size, k_value, starting_balance, gamma, num_timesteps)
 
     # Evaluation
     start_test = "2023-01-01"
     end_test = None
-    trained_model = f"models/{stock}_{model}"
+    trained_model = f"models/{stock}_A2C"
     evaluate(stock, start_test, end_test, trained_model, features, window_size, k_value, starting_balance)
+    trained_model = f"models/{stock}_PPO"
+    evaluate(stock, start_test, end_test, trained_model, features, window_size, k_value, starting_balance)
+
+    trained_model1 = f"models/{stock}_A2C"
+    trained_model2 = f"models/{stock}_PPO"
+    evaluate_both(stock, start_test, end_test, trained_model1, trained_model2, features, window_size, k_value, starting_balance)
